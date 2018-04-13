1633b4a51bdec203725aea490e793a65436e34ae
Samuel Terra <samuelterra22@hotmail.com>
2018-04-13 11:45:25 -0300

    Cria componente App

---
+++ src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ msg }}</div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        msg: 'Hello World'
+      };
+    },
+    created(){
+      console.log(this.$moment)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/web.entry.js
+++ src/web.entry.js
@@ -1,9 +1,18 @@
-import Vue from 'vue'
-import './style.scss'
+import Vue from 'vue';
+import './style.scss';
+
+import moment from 'moment-timezone'
+moment.tz.setDefault('UTC')
+Object.defineProperty(Vue.prototype, '$moment', {get() {return this.$root.moment}})
+
+import App from './components/App.vue';
 
 new Vue({
   el: '#app',
-  data: {
-    msg: 'Hello World'
+  data:{
+    moment
+  },
+  components: {
+    App
   }
 });
