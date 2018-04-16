9332f0e63c2a4b86bab6c3882de8826bb4b1867f
Samuel Terra <samuelterra22@hotmail.com>
2018-04-16 15:27:04 -0300

    Exibe e filtra eventos no calendario

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,5 +1,10 @@
 <template>
-    <div :class="classObject" @click="captureClick">{{day.format('D')}}</div>
+    <div :class="classObject" @click="captureClick">
+        {{day.format('D')}}
+        <ul class="event-list">
+            <li v-for="event in events">{{ event.description }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -7,6 +12,14 @@
     name: 'CalendarDay',
     props: ['day'],
     computed: {
+      events () {
+        let mockData = [
+          {description: 'Random event 1', date: this.$moment('2018-04-06', 'YYYY-MM-DD')},
+          {description: 'Random event 2', date: this.$moment('2018-04-15', 'YYYY-MM-DD')},
+          {description: 'Random event 3', date: this.$moment('2018-05-14', 'YYYY-MM-DD')}
+        ]
+        return mockData.filter(event => event.date.isSame(this.day, 'day'))
+      },
       classObject () {
         let today = this.day.isSame(this.$moment(), 'day')
         return {
@@ -16,8 +29,8 @@
         }
       }
     },
-    methods:{
-      captureClick(event){
+    methods: {
+      captureClick (event) {
         this.$store.commit('eventFormPos', {x: event.clientX, y: event.clientY})
         this.$store.commit('eventFormActive', true)
       }
