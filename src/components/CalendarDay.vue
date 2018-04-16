a3b6259d45987fe25e07371e80b9fc761f2a6d7c
Samuel Terra <samuelterra22@hotmail.com>
2018-04-16 13:42:48 -0300

    Adiciona efeitos de dias passados

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -5,12 +5,14 @@
 <script>
   export default {
     name: 'CalendarDay',
-    props:['day'],
-    computed:{
-      classObject(){
+    props: ['day'],
+    computed: {
+      classObject () {
+        let today = this.day.isSame(this.$moment(), 'day')
         return {
           day: true,
-          today: this.day.isSame(this.$moment(), 'day')
+          today,
+          past: this.day.isSameOrBefore(this.$moment(), 'day') && !today
         }
       }
     }
