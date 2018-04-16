e212808f2b50db80247c53a6a712ea42fff56b64
Samuel Terra <samuelterra22@hotmail.com>
2018-04-16 10:39:40 -0300

    Configura App para buscar a faixa correta de dias do mês

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,59 @@
 <template>
-    <div>{{ msg }}</div>
+    <div>
+        <div v-for="day in days"> {{day}}</div>
+    </div>
 </template>
 
 <script>
   export default {
     name: 'App',
-    data() {
+    data () {
       return {
-        msg: 'Hello World'
-      };
+        month: 5,
+        year: 2017
+      }
     },
-    created(){
-      console.log(this.$moment)
+    computed: {
+      days () {
+
+        // Generating all daus in current month
+        let days = []
+        let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D')
+
+        do {
+          days.push(currentDay)
+          currentDay = this.$moment(currentDay).add(1, 'days')
+        } while ((currentDay.month() + 1) === this.month)
+
+        // Add previus daus to start
+        currentDay = this.$moment(days[0])
+
+        const SUNDAY = 0
+        const MONDAY = 1
+
+        if (currentDay.day() !== MONDAY) {
+          do {
+            currentDay = this.$moment(currentDay).subtract(1, 'days')
+            days.unshift(currentDay)
+          } while (currentDay.day() !== MONDAY)
+        }
+
+
+
+        // Add following days do end
+        currentDay = this.$moment(days[days.length - 1])
+
+        if (currentDay.day() !== SUNDAY) {
+          do {
+            currentDay = this.$moment(currentDay).add(1, 'days')
+            days.push(currentDay)
+          } while (currentDay.day() !== SUNDAY)
+        }
+
+
+
+        return days
+      }
     }
   }
 </script>
