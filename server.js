06f75e035b2d31341f4d7c203ae7f24fefe9290a
Samuel Terra <samuelterra22@hotmail.com>
2018-04-16 22:50:19 -0300

    Configura bundle renderer

--- server.js
+++ server.js
@@ -18,9 +18,21 @@ let events = [
   {description: 'Random event 3', date: moment('2018-05-14', 'YYYY-MM-DD')}
 ]
 
+let renderer
+
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
   let contentMarker = '<!--APP-->'
+
+  if (renderer) {
+    renderer.renderToString({}, (err, html) =>{
+      if (err) {
+        console.log(err)
+      }else{
+        console.log(html)
+      }
+    })
+  }
   res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${serialize(events)}</script>`));
 
 });
@@ -38,7 +50,7 @@ if (process.env.NODE_ENV === 'development') {
   const reloadServer = reload(server, app);
   require('./webpack-dev-middleware').init(app);
   require('./webpack-server-compiler').init( function (bundle) {
-    console.log('Node bundle build')
+    renderer = require('vue-server-renderer').createBundleRenderer(bundle)
   })
 }
 
