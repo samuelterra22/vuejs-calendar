a789a00f1e2ed536cef997a1d1ce96458bc46d33
Samuel Terra <samuelterra22@hotmail.com>
2018-04-16 23:19:02 -0300

    Adiciona logo

--- server.js
+++ server.js
@@ -5,44 +5,51 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
-
-const moment = require('moment-timezone')
-moment.tz.setDefault('UTC')
-const serialize = require('serialize-javascript')
+const moment = require('moment-timezone');
+moment.tz.setDefault('UTC');
+const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 let events = [
-  {description: 'Random event 1', date: moment('2018-04-06', 'YYYY-MM-DD')},
-  {description: 'Random event 2', date: moment('2018-04-15', 'YYYY-MM-DD')},
-  {description: 'Random event 3', date: moment('2018-05-14', 'YYYY-MM-DD')}
-]
+  { description: 'Random event 1', date: moment('2018-04-06', 'YYYY-MM-DD') },
+  { description: 'Random event 2', date: moment('2018-04-15', 'YYYY-MM-DD') },
+  { description: 'Random event 3', date: moment('2018-05-14', 'YYYY-MM-DD') }
+];
 
-let renderer
+let renderer;
+
+if (process.env.NODE_ENV === 'production') {
+  let bundle = fs.readFileSync('./dist/node.bundle.js', 'utf8');
+  renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+  app.use('/dist', express.static(path.join(__dirname, 'dist')));
+}
 
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  let contentMarker = '<!--APP-->'
-
+  let contentMarker = '<!--APP-->';
   if (renderer) {
-    renderer.renderToString({events}, (err, html) =>{
+    renderer.renderToString({ events }, (err, html) => {
       if (err) {
-        console.log(err)
-      }else{
-        res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${serialize(events)}</script>\n${html}`));
+        console.log(err);
+      } else {
+        res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${ serialize(events) }</script>\n${html}`));
       }
-    })
-  }else {
-    res.send('<p>Awaiting compilation...</p>')
+    });
+  } else {
+    res.send('<p>Awaiting compilation..</p><script src="/reload/reload.js"></script>');
   }
-
 });
 
-app.use(require('body-parser').json())
+app.use(require('body-parser').json());
 app.post('/add_event', (req, res) => {
-  events.push(req.body)
-  res.sendStatus(200)
-})
+  let event = {
+    description: req.body.description,
+    date: moment(req.body.date)
+  };
+  events.push(event);
+  res.sendStatus(200);
+});
 
 const server = http.createServer(app);
 
@@ -50,9 +57,13 @@ if (process.env.NODE_ENV === 'development') {
   const reload = require('reload');
   const reloadServer = reload(server, app);
   require('./webpack-dev-middleware').init(app);
-  require('./webpack-server-compiler').init( function (bundle) {
-    renderer = require('vue-server-renderer').createBundleRenderer(bundle)
-  })
+  require('./webpack-server-compiler').init(function(bundle) {
+    let needsReload = (renderer === undefined);
+    renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+    if (needsReload) {
+      reloadServer.reload();
+    }
+  });
 }
 
 server.listen(process.env.PORT, function () {
