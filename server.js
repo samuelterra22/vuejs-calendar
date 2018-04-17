7fa7106f1c98cee7bdc5137c4690d305957a7d3c
Samuel Terra <samuelterra22@hotmail.com>
2018-04-16 22:03:54 -0300

    Adicionar os evento no html

--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const path = require('path');
 const fs = require('fs');
 const http = require('http');
 
+const moment = require('moment-timezone')
+moment.tz.setDefault('UTC')
+const serialize = require('serialize-javascript')
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
+let events = [
+  {description: 'Random event 1', date: moment('2018-04-06', 'YYYY-MM-DD')},
+  {description: 'Random event 2', date: moment('2018-04-15', 'YYYY-MM-DD')},
+  {description: 'Random event 3', date: moment('2018-05-14', 'YYYY-MM-DD')}
+]
 
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let contentMarker = '<!--APP-->'
+  res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${serialize(events)}</script>`));
 
 });
-
-let events = []
 
 app.use(require('body-parser').json())
 app.post('/add_event', (req, res) => {
--- src/web.entry.js
+++ src/web.entry.js
@@ -9,14 +9,14 @@ import App from './components/App.vue'
 moment.tz.setDefault('UTC')
 Object.defineProperty(Vue.prototype, '$moment', {get () {return this.$root.moment}})
 
-let events = [
-  {description: 'Random event 1', date: moment('2018-04-06', 'YYYY-MM-DD')},
-  {description: 'Random event 2', date: moment('2018-04-15', 'YYYY-MM-DD')},
-  {description: 'Random event 3', date: moment('2018-05-14', 'YYYY-MM-DD')}
-]
+let events = window.__INITIAL_STATE__.map(event => {
+  return {
+    description: event.description,
+    date: moment(event.date)
+  }
+})
 
 let initialState = Object.assign({}, store.state, {events})
-console.log(initialState)
 store.replaceState(initialState)
 
 new Vue({
